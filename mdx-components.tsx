e0fa8d63dda64514910881d4b8775651bea37439
benny <benny.nottonson01@student.csulb.edu>
2023-06-04 13:18:09 -0700

    fmt

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,7 @@
 import { PropsWithChildren } from "react";
 
-// This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: any): any {
 	return {
-		// Allows customizing built-in components, e.g. to add styling.
 		h1: ({ children }: PropsWithChildren) => (
 			<h1 className="mt-2 text-3xl font-bold tracking-tight text-zinc-100 md:text-center sm:text-4xl">
 				{children}
--- types/mdx.d.ts
+++ types/mdx.d.ts
@@ -1,4 +1,3 @@
-// types/mdx.d.ts
 declare module "*.mdx" {
 	let MDXComponent: (props) => JSX.Element;
 	export default MDXComponent;
