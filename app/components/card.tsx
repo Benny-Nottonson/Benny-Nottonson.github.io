abb0d419f417e6c319badfa240df7db9acd3727f
Benny Nottonson <bennynottonson@gmail.com>
2023-06-04 10:20:11 -0700

    imrpovement

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,9 +1,5 @@
 "use client";
-import {
-	motion,
-	useMotionTemplate,
-	useSpring,
-} from "framer-motion";
+import { motion, useMotionTemplate, useSpring } from "framer-motion";
 
 import { PropsWithChildren } from "react";
 
