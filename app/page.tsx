7f0ce0971785433ff6a7363d9751ff40b8ab4733
Benny Nottonson <bennynottonson@gmail.com>
2023-06-11 22:46:46 -0700

    acessability

--- app/page.tsx
+++ app/page.tsx
@@ -11,16 +11,18 @@ export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 			<nav className="my-16 animate-fade-in">
-				<ul className="flex items-center justify-center gap-4">
-					{navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
-					))}
+				<ul>
+					<li className="flex items-center justify-center gap-4">
+						{navigation.map((item) => (
+							<Link
+								key={item.href}
+								href={item.href}
+								className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+							>
+								{item.name}
+							</Link>
+						))}
+					</li>
 				</ul>
 			</nav>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
