377259664e13dda20a7138c741b0daa1d426258e
benny <benny.nottonson01@student.csulb.edu>
2023-09-02 16:20:17 -0700

    fix??

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -17,38 +17,34 @@ export const useProjects = routeLoader$(async () => {
 })
 
 export const project = server$(async () => {
-  const { readdirSync, readFileSync } = await import("fs");
-  const { join } = await import("path");
+  const projectFiles = import.meta.glob('./src/content/projects/*.mdx');
   const { default: matter } = await import("gray-matter");
 
-  const allProjects = readdirSync(
-    join(process.cwd(), "src/content/projects"),
-  ).map((fileName) => {
-    const slug = fileName.replace(/\.mdx$/, "");
-    const fileContents = readFileSync(
-      join(process.cwd(), "src/content/projects", fileName),
-      "utf8",
-    );
-    const { data } = matter(fileContents);
-    const date = data.date ? data.date : null;
-    const published = data.published ?? false;
-    const title = data.title ?? "";
-    const description = data.description ?? "";
-    const url = data.url ?? "";
-    const repository = data.repository ?? "";
-    const content = fileContents.split("---").slice(2).join("---");
-    return {
-      slug,
-      published,
-      date,
-      title,
-      description,
-      url,
-      repository,
-      content,
-      ...data,
-    };
-  });
+  const allProjects = await Promise.all(
+    Object.keys(projectFiles).map(async (fileName) => {
+      const slug = fileName.replace(/\.mdx$/, "");
+      const fileContents = await import(`./src/content/projects/${fileName}`).then((module) => module.default);
+      const { data } = matter(fileContents);
+      const date = data.date ? data.date : null;
+      const published = data.published ?? false;
+      const title = data.title ?? "";
+      const description = data.description ?? "";
+      const url = data.url ?? "";
+      const repository = data.repository ?? "";
+      const content = fileContents.split("---").slice(2).join("---");
+      return {
+        slug,
+        published,
+        date,
+        title,
+        description,
+        url,
+        repository,
+        content,
+        ...data,
+      };
+    })
+  );
 
   const projects = allProjects.reduce((acc: any, project) => {
     acc[project.slug] = project;
