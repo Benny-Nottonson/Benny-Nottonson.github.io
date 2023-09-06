f6c5f9f4a6e6138848532dd6889bf142eaf8b682
Benny Nottonson <bennynottonson@gmail.com>
2023-09-06 13:00:40 -0700

    template literals for consicion

--- src/components/projectCard/projectCard.tsx
+++ src/components/projectCard/projectCard.tsx
@@ -25,32 +25,32 @@ export default component$(({ project, isFeatured }: Props) => {
               )}
             </span>
           </div>
-          {!isFeatured ? (
-            <>
-              <h2 class="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-                {project.title}
-              </h2>
-              <p class="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                {project.description}
+          <h2
+            class={`group-hover:text-white font-display ${
+              isFeatured
+                ? "mt-4 text-3xl font-bold text-zinc-100  sm:text-4xl"
+                : "z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200"
+            }
+              `}
+          >
+            {project.title}
+          </h2>
+          <p
+            class={`text-zinc-400 mt-4 ${
+              isFeatured
+                ? "mt-4 leading-8 duration-150 group-hover:text-zinc-300"
+                : "z-20 mt-4 text-sm  duration-1000 group-hover:text-zinc-200"
+            }
+                `}
+          >
+            {project.description}
+          </p>
+          {isFeatured && (
+            <div class="pt-4 bottom-4 md:bottom-8">
+              <p class="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                Read more <span aria-hidden="true">&rarr;</span>
               </p>
-            </>
-          ) : (
-            <>
-              <h2
-                id="featured-post"
-                class="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
-              >
-                {project.title}
-              </h2>
-              <p class="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-                {project.description}
-              </p>
-              <div class="pt-4 bottom-4 md:bottom-8">
-                <p class="hidden text-zinc-200 hover:text-zinc-50 lg:block">
-                  Read more <span aria-hidden="true">&rarr;</span>
-                </p>
-              </div>
-            </>
+            </div>
           )}
         </article>
       </Card>
