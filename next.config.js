7fe1070ff51343f8d31ee86893dd439d1d8e6cc4
Benny-Nottonson <bennynottonson@gmail.com>
2025-02-16 11:47:41 -0800

    init rework
    Signed-off-by: Benny-Nottonson <bennynottonson@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    serverExternalPackages: [
+        '@react-email/render',
+    ]
+}
+
+module.exports = nextConfig
---
+++ src/components/HeroImage.tsx
@@ -0,0 +1,17 @@
+const HeroImage = ()  => {
+
+
+
+    return(
+        <>
+          <img
+          src={"https://github-readme-stats-private-two.vercel.app/api?username=benny-nottonson&hide_title=false&hide_rank=false&show_icons=true&include_all_commits=true&count_private=true&disable_animations=false&theme=dracula&locale=en&hide_border=false"}
+          alt="logo"
+          loading="eager"
+          height={1000}
+          width={1000}
+        />
+        </>
+    )
+}
+export default HeroImage
